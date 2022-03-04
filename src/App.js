6e9a67189fe626193869f437bcefb4378ee013aa
NamDo8467 <huynhnamdo@gmail.com>
2022-03-03 19:30:15 -0500

    add Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Footer from "./Footer/Footer.jsx"
+import Home from "./ContentContainer/Home"
+import {Routes, Route } from "react-router-dom"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App container">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="all" element={<Footer />} />
+                <Route
+                    path="*"
+                    element={
+                        <main style={{ padding: "1rem" }}>
+                            <p>There's nothing here!</p>
+                        </main>
+                    }
+                />
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
