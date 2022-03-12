7c128aea3cd80dc843b8b6fa5be4518884ebe5a6
NamDo8467 <huynhnamdo@gmail.com>
2022-03-11 20:46:56 -0500

    add PromotionCard

--- src/ContentContainer/CardContainer.jsx
+++ src/ContentContainer/CardContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 function CardContainer({ children }) {
-    return <div className="row justify-content-center">{children}</div>
+    return <div className="row container justify-content-center align-items-end">{children}</div>
 }
 
 export default CardContainer
---
+++ src/ContentContainer/PromotionCard.jsx
@@ -0,0 +1,73 @@
+import React from "react"
+
+function PromotionCard({ image, category, title, flexDirection }) {
+    const width = "auto"
+    const height = "100%"
+    const listStyle = {
+        fontSize: "13px",
+        listStyle: "none",
+        paddingLeft: "0px",
+    }
+    if (flexDirection === "column") {
+        return (
+            <div className="col-sm-3">
+                <div className="">
+                    <div className="card-body row">
+                        <div className="col-7">
+                            <img
+                                className="card-img"
+                                width={width}
+                                height={height}
+                                src={image}
+                                alt="Card cap"
+                            />
+                        </div>
+                        <div className="col-5">
+                            <h6 className="card-title category">{category}</h6>
+                            <h5 className="card-title title">{title}</h5>
+                            <ul style={listStyle}>
+                                <li>Hot deals</li>
+                                <li>Sale up to 50%</li>
+                                <li>
+                                    <a href="/">View more</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    } else if (flexDirection === "row") {
+        return (
+            <div className="col-sm-3">
+                <div className="">
+                    <div className="card-body row">
+                        <div className="row mb-5">
+                            <img
+                                className="card-img"
+                                width={width}
+                                height={height}
+                                src={image}
+                                alt="Card cap"
+                            />
+                        </div>
+
+                        <div className="row text-center">
+                            <h6 className="card-title category">{category}</h6>
+                            <h5 className="card-title title">{title}</h5>
+                            <ul style={listStyle}>
+                                <li>Hot deals</li>
+                                <li>Sale up to 50%</li>
+                                <li>
+                                    <a href="/">View more</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default PromotionCard
