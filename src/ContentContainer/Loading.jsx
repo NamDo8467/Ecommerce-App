896603f78bff6cf1c7582c49ad75ba84eb1e1f26
NamDo8467 <huynhnamdo@gmail.com>
2022-03-11 20:47:33 -0500

    change Loading message

--- src/ContentContainer/Loading.jsx
+++ src/ContentContainer/Loading.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react"
 
-function Loading({children}) {
-  return (
-	<div>{children}</div>
-  )
+function Loading({ children }) {
+    return <h1 className="h-1">{children}</h1>
 }
 
 export default Loading
