26ae0a666ccb6a3ab54134a54160bd2ff60df446
NamDo8467 <huynhnamdo@gmail.com>
2022-06-06 16:49:29 -0400

    change header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,21 +1,29 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useContext } from "react"
+import CartContext from "../CartContext"
+
 function Header() {
+    const { itemList } = useContext(CartContext)
+    console.log(itemList)
     return (
         <header>
             <div className="search d-flex justify-content-between py-3">
                 <img className="navbar-brand col-6" src="/sfh.jpg" alt="logo" />
                 <form className="d-flex col-3 pe-3">
-                    <input
-                        className="form-control me-2"
-                        type="search"
-                        placeholder="Search"
-                        aria-label="Search"
-                    />
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                     <button className="btn btn-outline-primary" type="submit">
                         Search
                     </button>
                 </form>
+                <Link className="nav-link" to="/cart">
+                    <h4>
+                        <i className="bi bi-bag-fill" style={{ color: "black", fontStyle: "normal" }}>
+                            &nbsp; &nbsp;
+                            <span>{itemList.length}</span>
+                        </i>
+                    </h4>
+                </Link>
             </div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
@@ -30,36 +38,24 @@ function Header() {
                     >
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div
-                        className="collapse navbar-collapse d-flex justify-content-center"
-                        id="navbarNav"
-                    >
+                    <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
                         <ul className="navbar-nav">
-                            <li className="nav-item mx-3">
-                                <Link className="nav-link" to="/">
-                                    Home
-                                </Link>
-                            </li>
-                            <li className="nav-item mx-3">
-                                <Link className="nav-link" to="electronics">
-                                    Electronics
-                                </Link>
-                            </li>
-                            <li className="nav-item mx-3">
-                                <Link className="nav-link" to="jewelry">
-                                    Jewelry
-                                </Link>
-                            </li>
-                            <li className="nav-item mx-3">
-                                <Link className="nav-link" to="menclothing">
-                                    Men's clothing
-                                </Link>
-                            </li>
-                            <li className="nav-item mx-3">
-                                <Link className="nav-link" to="womenclothing">
-                                    Women's clothing
-                                </Link>
-                            </li>
+                            <Link className="nav-link" to="/">
+                                <li className="nav-item mx-3">Home</li>
+                            </Link>
+
+                            <Link className="nav-link" to="/category/electronics">
+                                <li className="nav-item mx-3">Electronics</li>
+                            </Link>
+                            <Link className="nav-link" to="/category/jewelery">
+                                <li className="nav-item mx-3">Jewelry</li>
+                            </Link>
+                            <Link className="nav-link" to="/category/menclothing">
+                                <li className="nav-item mx-3">Men's clothing</li>
+                            </Link>
+                            <Link className="nav-link" to="/category/womenclothing">
+                                <li className="nav-item mx-3">Women's clothing</li>
+                            </Link>
                         </ul>
                     </div>
                 </div>
