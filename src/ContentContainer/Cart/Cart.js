cf4d42ade012daf20b5d53aed94d792c338d06db
NamDo8467 <huynhnamdo@gmail.com>
2022-06-13 14:01:16 -0400

    add style and logic

--- src/ContentContainer/Cart/Cart.js
+++ src/ContentContainer/Cart/Cart.js
@@ -1,10 +1,46 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import CartContext from "../../CartContext"
 import Header from "../../Header/Header"
 import Footer from "../../Footer/Footer"
+import "./Cart.css"
 function Cart() {
-    const { itemList } = useContext(CartContext)
-    console.log(itemList)
+    const { itemList, addItemToCart, deleteItemFromCart, removeItemFromCart } = useContext(CartContext)
+    const SAVE_PRICE = 5.95
+    let date = new Date()
+    const [quantity, setQuantity] = useState(1)
+    const [total, setTotal] = useState(0)
+    const getMonthInitials = () => {
+        switch (date.getMonth()) {
+            case 0:
+                return "Jan"
+            case 1:
+                return "Feb"
+            case 2:
+                return "Mar"
+            case 3:
+                return "Apr"
+            case 4:
+                return "May"
+            case 5:
+                return "Jun"
+            case 6:
+                return "Jul"
+            case 7:
+                return "Aug"
+            case 8:
+                return "Sep"
+            case 9:
+                return "Oct"
+            case 10:
+                return "Nov"
+            case 11:
+                return "Dec"
+
+            default:
+                break
+        }
+    }
+
     if (itemList.length === 0 || itemList === undefined || itemList == null) {
         return (
             <div>
@@ -28,7 +64,87 @@ function Cart() {
             </div>
         )
     }
-    return <div>Cart</div>
+    let totalPrice = 0
+    itemList.forEach(item => {
+        totalPrice += item[0].price * item[1].quantity
+    })
+
+    const increaseQuantity = item => {
+        addItemToCart(item)
+    }
+
+    return (
+        <div>
+            <Header />
+            <div className="row">
+                <section className="product-information col-9">
+                    {itemList.map(item => {
+                        // setTotal(prevTotal => prevTotal + item[0].price)
+                        return (
+                            <div key={item[0].id} className="product pb-3 d-flex pt-3 border justify-content-center flex-wrap">
+                                <img className="col-2 me-5" src={item[0].image} alt="product" />
+                                <p className="col-7">{item[0].title}</p>
+                                <div className="col-2 price">
+                                    <span style={{ fontSize: "13px", color: "#c21313", fontWeight: "bold" }}>
+                                        SAVE ${(item[0].price * 10) / 100 + SAVE_PRICE}
+                                    </span>
+                                    <h5 className="text-danger" style={{ fontWeight: "bold" }}>
+                                        ${item[0].price}
+                                    </h5>
+                                    <p className="text-muted" style={{ fontSize: "13px" }}>
+                                        SALE ends: {getMonthInitials()} {date.getDate()}, {date.getFullYear()}
+                                    </p>
+                                </div>
+                                <div className="quantity-setter col-4">
+                                    {item[1].quantity == 1 ? (
+                                        <button className="btn btn-dark p-1 disabled">
+                                            <i className="bi bi-dash"></i>
+                                        </button>
+                                    ) : (
+                                        <button
+                                            className="btn btn-dark p-1"
+                                            onClick={() => {
+                                                deleteItemFromCart(item[0])
+                                            }}
+                                        >
+                                            <i className="bi bi-dash"></i>
+                                        </button>
+                                    )}
+                                    &nbsp;&nbsp;&nbsp;&nbsp;
+                                    {item[1].quantity}
+                                    &nbsp;&nbsp;&nbsp;&nbsp;
+                                    <button
+                                        className="btn btn-primary p-1"
+                                        onClick={() => {
+                                            addItemToCart(item[0])
+                                        }}
+                                    >
+                                        <i className="bi bi-plus"></i>
+                                    </button>
+                                    <br />
+                                    <br />
+                                    <div>
+                                        <i
+                                            className="bi bi-trash3"
+                                            onClick={() => {
+                                                removeItemFromCart(item[0])
+                                            }}
+                                        >
+                                            &nbsp; <p style={{display: "inline" }}>Remove</p>
+                                        </i>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                    <h5>Product Total: ${totalPrice}</h5>
+                </section>
+
+                <section className="order-summary col-4"></section>
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
 export default Cart
