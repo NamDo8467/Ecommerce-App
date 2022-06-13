1791ea12c3487369348f0525111405dc7a09f7ff
NamDo8467 <huynhnamdo@gmail.com>
2022-06-13 14:02:27 -0400

    add functions to context

--- src/CartContext.js
+++ src/CartContext.js
@@ -5,10 +5,37 @@ const CartContext = createContext()
 export default CartContext
 
 export function CartProvider({ children }) {
-	const [itemList, setItemList] = useState([])
-	
-	const addItemToCart = (item) => {
-		setItemList((prevItemList)=> [...prevItemList, item])
-	}
-    return <CartContext.Provider value={{itemList, addItemToCart}}>{children}</CartContext.Provider>
+    //Each item in the itemList is an array with 2 elements: {the item information} and {quantity}
+    const [itemList, setItemList] = useState([])
+
+    const addItemToCart = item => {
+        const product = itemList.find(product => product[0].title === item.title)
+        let itemListClone = [...itemList] // make a copy of itemList
+        if (product === undefined) {
+            setItemList(prevItemList => [...prevItemList, [item, { quantity: 1 }]])
+        } else {
+            itemListClone[itemList.findIndex(product => product[0].title === item.title)][1].quantity += 1
+            setItemList(itemListClone)
+        }
+    }
+    const deleteItemFromCart = item => {
+        let itemListClone = [...itemList] // make a copy of itemList
+
+        itemListClone[itemList.findIndex(product => product[0].title === item.title)][1].quantity -= 1
+        setItemList(itemListClone)
+    }
+
+    const removeItemFromCart = item => {
+        let itemListClone = [...itemList] // make a copy of itemList
+
+        const index = itemListClone.findIndex(product => product[0].title === item.title)
+
+        itemListClone.splice(index, 1)
+        setItemList(itemListClone)
+    }
+    return (
+        <CartContext.Provider value={{ itemList, addItemToCart, deleteItemFromCart, removeItemFromCart  }}>
+            {children}
+        </CartContext.Provider>
+    )
 }
